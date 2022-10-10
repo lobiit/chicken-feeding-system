00738b7536e1cf3d9dbc73e0a6d8eb999d7dc3be
Tumbo Lobi John <johnlobiit@gmail.com>
2022-10-10 15:13:17 +0300

    created feeder model and form

---
+++ feeder/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Feeder
+
+
+class FeederForm(ModelForm):
+    class Meta:
+        model = Feeder
+        fields = ['feed_per_hen', 'number_of_chicken', 'feeder_opened']
