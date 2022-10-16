8dc00ef3085d5c97638be1a4af49e51cc884dbe1
Tumbo Lobi John <johnlobiit@gmail.com>
2022-10-16 12:10:04 +0300

    saving property results to a db column

--- feeder/models.py
+++ feeder/models.py
@@ -3,20 +3,16 @@ from django.db import models
 
 # Create your models here.
 class FeederData(models.Model):
-    total_amount_of_feed = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    total_amount_of_feed = models.FloatField(default=0, blank=True)
     feed_per_hen = models.DecimalField(default=70.00, max_digits=20, decimal_places=2)
     number_of_chicken = models.IntegerField(default=100)
     feeder_opened = models.BooleanField(default=False)
-    # amount_of_feeds_refill = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    amount_of_feeds_refill = models.FloatField(default=0, blank=True)
 
     @property
-    def amount_released(self):
-        return "%.2f" % (float(self.feed_per_hen) * float(self.number_of_chicken))
+    def total_available_feed(self):
+        return "%.2f" % (float(self.total_amount_of_feed) + float(self.amount_of_feeds_refill))
 
-    @property
-    def remaining_feed(self):
-        return "%.2f" % (float(self.total_amount_of_feed) - float(self.amount_released))
-
-    # @property
-    # def feeder_refill(self):
-    #     return "%.2f" % (float(self.remaining_feed) + float(self.amount_of_feeds_refill))
+    def save(self, *args, **kwargs):
+        self.total_amount_of_feed = self.total_available_feed
+        super(FeederData, self).save(*args, **kwargs)
