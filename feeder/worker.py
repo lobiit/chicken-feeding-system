93981e62e861f4630b8cd0dfcb974a3e7257e553
Tumbo Lobi John <johnlobiit@gmail.com>
2022-10-16 08:44:13 +0300

    worker update

--- feeder/worker.py
+++ feeder/worker.py
@@ -15,8 +15,9 @@ def worker():
 
         postgres_insert_query = """ 
         update  public.api_feederdata
-        set feeder_opened=TRUE
-        from  public.api_feederdata as s
+        set total_amount_of_feed=total_amount_of_feed-(number_of_chicken * feed_per_hen)
+        where total_amount_of_feed>=(number_of_chicken * feed_per_hen)
+        from  public.api_feederdata 
         """
         query2 = """update  public.api_feederdata
         set feeder_opened=true
@@ -40,9 +41,50 @@ def worker():
             print("PostgreSQL connection is closed")
 
 
+def terminate():
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="1000",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="feeder")
+        cursor = connection.cursor()
+        print("Connected...")
+
+        postgres_insert_query = """ 
+        update  public.api_feederdata
+        set feeder_opened=FALSE
+        from  public.api_feederdata as s
+        """
+        query2 = """update  public.api_feederdata
+        set feeder_opened=FALSE
+        where feeder_opened=false """
+        cursor.execute(postgres_insert_query)
+        cursor.execute(query2)
+
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Feeder closed ...")
+
+
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to open feeder", error)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
 schedule.every().day.at("09:00").do(worker)
+schedule.every().day.at("09:30").do(terminate)
+
+schedule.every().day.at("17:00").do(worker)
+schedule.every().day.at("17:30").do(terminate)
 
 while True:
-    worker()
+    schedule.run_pending()
     time.sleep(1)
     print("Worker/cron running")
